546d146a3b8f3bd9e32eb81a01d156267cf2c7f3
Carlos G. Camacho O <carloscgo123@gmail.com>
2018-11-16 10:48:49 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Facades\Route;
 /**
  * This route is called when user must first time confirm secret
  */
-Route::post('register', 'Lifeonscreen\Google2fa\Google2fa@register');
+Route::post('register', 'CarlosCGO\Google2fa\Google2fa@register');
 
 /**
  * This route is called when user must first time confirm secret
  */
-Route::post('confirm', 'Lifeonscreen\Google2fa\Google2fa@confirm');
+Route::post('confirm', 'CarlosCGO\Google2fa\Google2fa@confirm');
 
 /**
  * This route is called to verify users secret
  */
-Route::post('authenticate', 'Lifeonscreen\Google2fa\Google2fa@authenticate');
+Route::post('authenticate', 'CarlosCGO\Google2fa\Google2fa@authenticate');
